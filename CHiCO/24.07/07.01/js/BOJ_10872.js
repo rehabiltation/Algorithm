3de971fd321887f10c01f0ba27bac2e78010bd21
chico <silverstuff@naver.com>
2024-07-02 01:25:26 +0900

    [chico] 07.01

---
+++ CHiCO/24.07/07.01/js/BOJ_10872.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const input = fs.readFileSync('./BOJ_10872.txt').toString().split(' ')
+
+const A = parseInt(input[0])
+
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(A))
